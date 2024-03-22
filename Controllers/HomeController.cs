381bbe0fcf70ac70a02c16f118c49775d9d79be7
aliogl2112 <you@aliogul2112@gmail.com>
2024-03-23 00:34:20 +0300

    feature: added ViewBag and ViewData

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,6 +5,9 @@ namespace MeetingApp.Controllers
     public class HomeController : Controller
     {
         public IActionResult Index(){
+            int time = DateTime.Now.Hour;
+            ViewBag.Message = time>12?"İyi Günler":"Günaydın";
+            ViewData["username"]="Ali";
             return View();
         }
     }
